72117e74b83898d779065e429e78968594e0f354
alebelcor <856838+alebelcor@users.noreply.github.com>
2012-04-28 18:00:36 -0700

    First commit.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    'use strict';
+
+    var dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+        monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+                'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        loadRates = $.getJSON('http://openexchangerates.org/latest.json', function (json) {
+            var timestamp = new Date(json.timestamp * 1000),
+                day = dayNames[timestamp.getDay()],
+                month = monthNames[timestamp.getMonth()];
+
+            $('#price').text('El precio del dólar es de $' + json.rates.MXN + ' MXN');
+            $('#when').text('Cuando: '
+                + day + ', '
+                + month + ' '
+                + timestamp.getDate() + ' '
+                + timestamp.getFullYear() + ', '
+                + timestamp.toLocaleTimeString());
+        });
+
+    loadRates();
+});
