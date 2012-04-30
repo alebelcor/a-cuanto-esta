417ee9e9a6be895648ddd5b99ada9c3534acaa07
alebelcor <856838+alebelcor@users.noreply.github.com>
2012-04-29 21:16:38 -0700

    Added some bootstrap styling and fauxness

--- js/main.js
+++ js/main.js
@@ -8,15 +8,21 @@ $(function () {
             $.getJSON('http://openexchangerates.org/latest.json', function (json) {
                 var timestamp = new Date(json.timestamp * 1000),
                     day = dayNames[timestamp.getDay()],
-                    month = monthNames[timestamp.getMonth()];
+                    month = monthNames[timestamp.getMonth()],
+                    usdPane$ = $('#usd-pane');
 
-                $('#price').text('El precio del dólar es de $' + json.rates.MXN + ' MXN');
-                $('#when').text('Cuando: '
-                    + day + ', '
-                    + month + ' '
-                    + timestamp.getDate() + ' '
-                    + timestamp.getFullYear() + ', '
-                    + timestamp.toLocaleTimeString());
+                $('<p></p>').html('El precio del dólar es de <strong>$' + json.rates.MXN + '</strong> MXN')
+                    .appendTo(usdPane$);
+                $('<p></p>').text('Última actualización: ' + day + ', ' + month + ' ' + timestamp.getDate()
+                    + ' ' + timestamp.getFullYear() + ', ' + timestamp.toLocaleTimeString())
+                    .appendTo(usdPane$);
+
+                //$('#annotation').toggleClass('invisible');
+                $('#annotation').fadeToggle();
+            }).error(function () {
+                $('#annotation').find('h3')
+                    .html('<span class="label label-important">Error</span>&thinsp;'
+                        + 'Oh oh, hubo algún error. Intenta de nuevo luego.');
             });
         };
 
