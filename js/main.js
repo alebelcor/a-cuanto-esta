f67ed6bae0bca5093e30e5a4960b4a248e62eef1
alebelcor <856838+alebelcor@users.noreply.github.com>
2012-05-04 17:53:27 -0700

    Changed indentation to tabs

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 $(function () {
-    'use strict';
+	'use strict';
 
-    var dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-        monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
-                'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        loadRates = function () {
-            $.getJSON('http://openexchangerates.org/latest.json', function (json) {
-                var timestamp = new Date(json.timestamp * 1000),
-                    day = dayNames[timestamp.getDay()],
-                    month = monthNames[timestamp.getMonth()],
-                    usdPane$ = $('#usd-pane');
+	var dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+		monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+				'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+		loadRates = function () {
+			$.getJSON('http://openexchangerates.org/latest.json', function (json) {
+				var timestamp = new Date(json.timestamp * 1000),
+					day = dayNames[timestamp.getDay()],
+					month = monthNames[timestamp.getMonth()],
+					usdPane$ = $('#usd-pane');
 
-                $('<p></p>').html('El precio del dólar es de <strong>$' + json.rates.MXN + '</strong> MXN')
-                    .appendTo(usdPane$);
-                $('<p></p>').text('Última actualización: ' + day + ', ' + month + ' ' + timestamp.getDate()
-                    + ' ' + timestamp.getFullYear() + ', ' + timestamp.toLocaleTimeString())
-                    .appendTo(usdPane$);
-                $('#annotation').fadeToggle('slow');
+				$('<p></p>').html('El precio del dólar es de <strong>$' + json.rates.MXN + '</strong> MXN')
+					.appendTo(usdPane$);
+				$('<p></p>').text('Última actualización: ' + day + ', ' + month + ' ' + timestamp.getDate()
+					+ ' ' + timestamp.getFullYear() + ', ' + timestamp.toLocaleTimeString())
+					.appendTo(usdPane$);
+				$('#annotation').fadeToggle('slow');
 
-            }).error(function () {
-                $('#annotation')
-                    .fadeToggle('slow', function () {
-                        $(this).html('<span class="label label-important">Error</span>&nbsp;'
-                        + 'Oh oh, algo no anda bien. Intenta de nuevo luego.');
-                    }).fadeIn('slow');
-                $('.tabbable').fadeToggle('slow');
-            });
-        };
+			}).error(function () {
+				$('#annotation')
+					.fadeToggle('slow', function () {
+						$(this).html('<span class="label label-important">Error</span>&nbsp;'
+						+ 'Oh oh, algo no anda bien. Intenta de nuevo luego.');
+					}).fadeIn('slow');
+				$('.tabbable').fadeToggle('slow');
+			});
+		};
 
-    $(document.documentElement).removeClass('no-js').addClass('js');
-    $('#annotation').html('<span>Cargando información... </span>'
-        + '<img src="img/loader.gif" alt="ícono de cargador" title="Cargando...">');
-    loadRates();
+	$(document.documentElement).removeClass('no-js').addClass('js');
+	$('#annotation').html('<span>Cargando información... </span>'
+		+ '<img src="img/loader.gif" alt="ícono de cargador" title="Cargando...">');
+	loadRates();
 
 });
