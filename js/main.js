f47480200335ae1aff4f733ceae26bd79780d94e
alebelcor <856838+alebelcor@users.noreply.github.com>
2012-05-09 20:48:04 -0700

    Changed Ajax call to avoid caching. Also, changes for issue #9

--- js/main.js
+++ js/main.js
@@ -20,40 +20,53 @@
 			annotation$.html('<span>Cargando información... </span>'
 				+ '<img src="img/loader.gif" alt="ícono de cargador" title="Cargando...">');
 
-			$.getJSON('http://openexchangerates.org/latest.json', function (json) {
-				var timestamp = new Date(json.timestamp * 1000),
-					day = dayNames[timestamp.getDay()],
-					month = monthNames[timestamp.getMonth()],
-					lastUpdate = 'Última actualización: ' + day + ', ' + month + ' ' + timestamp.getDate()
-						+ ' ' + timestamp.getFullYear() + ', ' + timestamp.toLocaleTimeString(),
-					usdPane$ = $('#usd-pane'),
-					eurPane$ = $('#eur-pane'),
-					gbpPane$ = $('#gbp-pane');
+			$.ajax({
+				url: 'http://openexchangerates.org/latest.json',
+				dataType: 'json',
+				cache: false,
+				success: function (json) {
+					var usdPane$ = $('#usd-pane'),
+						eurPane$ = $('#eur-pane'),
+						gbpPane$ = $('#gbp-pane'),
 
-				fx.rates = json.rates;
-				fx.base = json.base;
+						timestamp = new Date(json.timestamp * 1000),
 
-				$('<p></p>').html('El precio del dólar es de <strong>$'
-					+ fx(1).from('USD').to('MXN').toFixed(4) + '</strong> MXN').appendTo(usdPane$);
-				$('<p></p>').text(lastUpdate).appendTo(usdPane$);
+						day = dayNames[timestamp.getDay()],
+						month = monthNames[timestamp.getMonth()],
+						hours = timestamp.getHours(),
+						hoursTranslated = hours > 12 ?
+								(hours === 12 ? '12 PM' : (hours - 12) + ' PM') :
+								(hours === 0 ? '12 AM' : hours + ' AM'),
 
-				$('<p></p>').html('El precio del euro es de <strong>$'
-					+ fx(1).from('EUR').to('MXN').toFixed(4) + '</strong> MXN').appendTo(eurPane$);
-				$('<p></p>').text(lastUpdate).appendTo(eurPane$);
+						lastUpdate = 'Última actualización: ' + day + ', ' + month + ' '
+							+ timestamp.getDate() + ' ' + timestamp.getFullYear() + ', '
+							+ hoursTranslated;
 
-				$('<p></p>').html('El precio de la libra esterlina es de <strong>$'
-					+ fx(1).from('GBP').to('MXN').toFixed(4) + '</strong> MXN').appendTo(gbpPane$);
-				$('<p></p>').text(lastUpdate).appendTo(gbpPane$);
+					fx.rates = json.rates;
+					fx.base = json.base;
 
-				annotation$.fadeToggle('slow');
+					$('<p></p>').html('El precio del dólar es de <strong>$'
+						+ fx(1).from('USD').to('MXN').toFixed(4) + '</strong> MXN').appendTo(usdPane$);
+					$('<p></p>').text(lastUpdate).appendTo(usdPane$);
 
-			}).error(function () {
-				annotation$
-					.fadeToggle('slow', function () {
+					$('<p></p>').html('El precio del euro es de <strong>$'
+						+ fx(1).from('EUR').to('MXN').toFixed(4) + '</strong> MXN').appendTo(eurPane$);
+					$('<p></p>').text(lastUpdate).appendTo(eurPane$);
+
+					$('<p></p>').html('El precio de la libra esterlina es de <strong>$'
+						+ fx(1).from('GBP').to('MXN').toFixed(4) + '</strong> MXN').appendTo(gbpPane$);
+					$('<p></p>').text(lastUpdate).appendTo(gbpPane$);
+
+					annotation$.fadeToggle('slow');
+
+				},
+				error: function () {
+					annotation$.fadeToggle('slow', function () {
 						$(this).html('<span class="label label-important">Error</span>&nbsp;'
 							+ 'Oh oh, algo no anda bien. Intenta de nuevo luego.');
 					}).fadeIn('slow');
-				$('.tabbable').fadeToggle('slow');
+					$('.tabbable').fadeToggle('slow');
+				}
 			});
 		};
 
