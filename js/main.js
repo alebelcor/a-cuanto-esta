c7c22bf25967132630f3362dc3c2fd126f739d04
alebelcor <856838+alebelcor@users.noreply.github.com>
2012-05-20 16:42:41 -0700

    Changed the loading of the exchange rates to a subscription.

--- js/main.js
+++ js/main.js
@@ -1,75 +1,14 @@
 ;(function ($, window, undefined) {
 	'use strict';
 
-	var App = {},
-
+	var document = window.document,
+		// jQuery Tiny Pub/Sub
 		o = $({}),
-
-		document = window.document,
-
-		loadRates = function () {
-			var dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-				monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
-					'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-				annotation$ = $('#annotation');
-
-			annotation$.html('<span>Cargando información... </span>'
-				+ '<img src="img/loader.gif" alt="ícono de cargador" title="Cargando...">');
-
-			$.ajax({
-				url: '//openexchangerates.org/latest.json',
-				dataType: 'json',
-				cache: false,
-				success: function (json) {
-					var usdPane$ = $('#usd-pane'),
-						eurPane$ = $('#eur-pane'),
-						gbpPane$ = $('#gbp-pane'),
-
-						timestamp = new Date(json.timestamp * 1000),
-
-						day = dayNames[timestamp.getDay()],
-						month = monthNames[timestamp.getMonth()],
-						hours = timestamp.getHours(),
-						hoursTranslated = hours > 12 ?
-								(hours === 12 ? '12 PM' : (hours - 12) + ' PM') :
-								(hours === 0 ? '12 AM' : hours + ' AM'),
-
-						lastUpdate = 'Última actualización: ' + day + ', ' + month + ' '
-							+ timestamp.getDate() + ' ' + timestamp.getFullYear() + ', '
-							+ hoursTranslated;
-
-					fx.rates = json.rates;
-					fx.base = json.base;
-
-					$('<p></p>').html('El precio del dólar es de <strong>$'
-						+ fx(1).from('USD').to('MXN').toFixed(4) + '</strong> MXN').appendTo(usdPane$);
-					$('<p></p>').text(lastUpdate).appendTo(usdPane$);
-
-					$('<p></p>').html('El precio del euro es de <strong>$'
-						+ fx(1).from('EUR').to('MXN').toFixed(4) + '</strong> MXN').appendTo(eurPane$);
-					$('<p></p>').text(lastUpdate).appendTo(eurPane$);
-
-					$('<p></p>').html('El precio de la libra esterlina es de <strong>$'
-						+ fx(1).from('GBP').to('MXN').toFixed(4) + '</strong> MXN').appendTo(gbpPane$);
-					$('<p></p>').text(lastUpdate).appendTo(gbpPane$);
-
-					annotation$.fadeToggle('slow');
-
-				},
-				error: function () {
-					annotation$.fadeToggle('slow', function () {
-						$(this).html('<span class="label label-important">Error</span>&nbsp;'
-							+ 'Oh oh, algo no anda bien. Intenta de nuevo luego.');
-					}).fadeIn('slow');
-					$('.tabbable').fadeToggle('slow');
-				}
-			});
+		App = {
+			publish: function () { o.trigger.apply(o, arguments); },
+			subscribe: function () { o.on.apply(o, arguments); },
+			unsubscribe: function () { o.off.apply(o, arguments); }
 		};
-
-	// jQuery Tiny Pub/Sub
-	App.publish = function () { o.trigger.apply(o, arguments); };
-	App.subscribe = function () { o.on.apply(o, arguments); };
-	App.unsubscribe = function () { o.off.apply(o, arguments); };
 
 	// Subscriptions/events
 	App.subscribe('enable-js-class', function () {
@@ -116,10 +55,69 @@
 
 	});
 
+	App.subscribe('load-exchange-rates', function () {
+		var dayNames = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+			monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto',
+				'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+			annotation$ = $('#annotation');
+
+		annotation$.html('<span>Cargando información... </span>'
+			+ '<img src="img/loader.gif" alt="ícono de cargador" title="Cargando...">');
+
+		$.ajax({
+			url: '//openexchangerates.org/latest.json',
+			dataType: 'json',
+			cache: false,
+			success: function (json) {
+				var usdPane$ = $('#usd-pane'),
+					eurPane$ = $('#eur-pane'),
+					gbpPane$ = $('#gbp-pane'),
+
+					timestamp = new Date(json.timestamp * 1000),
+
+					day = dayNames[timestamp.getDay()],
+					month = monthNames[timestamp.getMonth()],
+					hours = timestamp.getHours(),
+					hoursTranslated = hours > 12 ?
+							(hours === 12 ? '12 PM' : (hours - 12) + ' PM') :
+							(hours === 0 ? '12 AM' : hours + ' AM'),
+
+					lastUpdate = 'Última actualización: ' + day + ', ' + month + ' '
+						+ timestamp.getDate() + ' ' + timestamp.getFullYear() + ', '
+						+ hoursTranslated;
+
+				fx.rates = json.rates;
+				fx.base = json.base;
+
+				$('<p></p>').html('El precio del dólar es de <strong>$'
+					+ fx(1).from('USD').to('MXN').toFixed(4) + '</strong> MXN').appendTo(usdPane$);
+				$('<p></p>').text(lastUpdate).appendTo(usdPane$);
+
+				$('<p></p>').html('El precio del euro es de <strong>$'
+					+ fx(1).from('EUR').to('MXN').toFixed(4) + '</strong> MXN').appendTo(eurPane$);
+				$('<p></p>').text(lastUpdate).appendTo(eurPane$);
+
+				$('<p></p>').html('El precio de la libra esterlina es de <strong>$'
+					+ fx(1).from('GBP').to('MXN').toFixed(4) + '</strong> MXN').appendTo(gbpPane$);
+				$('<p></p>').text(lastUpdate).appendTo(gbpPane$);
+
+				annotation$.fadeToggle('slow');
+
+			},
+			error: function () {
+				annotation$.fadeToggle('slow', function () {
+					$(this).html('<span class="label label-important">Error</span>&nbsp;'
+						+ '<em>Oh oh, algo no anda bien. Intenta de nuevo luego.</em>');
+				}).fadeIn('slow');
+				$('.tabbable').fadeToggle('slow');
+			}
+		});
+	});
+
 	App.subscribe('init', function () {
 		App.publish('enable-js-class');
 		App.publish('create-share-buttons');
-		loadRates();
+		App.publish('load-exchange-rates');
 	});
 
 	$(function ($) {
